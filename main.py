7a8069950e2d0ba76ab2eebe2b0d54521e5fe768
SHDWCr1tical <77359556+SHDWCr1tical@users.noreply.github.com>
2021-01-28 12:00:27 -0500

    Quick bug fix.
    Fixed some bugs keeping the bot from running.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ mainshop = [{"name":"Watch","price":500,"description":"Time"},
             {"name":"Rifle","price":20000,"description":"Hunting"}]
 
 
-client = commands.client(command_prefix = '>')
+client = commands.Bot(command_prefix = '>')
 
 
 @client.event
@@ -25,24 +25,10 @@ async def on_command_error(ctx, error):
 
 @client.event
 async def on_ready():
-    await client.change_presence(statuses)
+    await client.change_presence(activity=discord.Game(name='with the ban hammer.'))
     print('Logged in as:')
     print(client.user.name)
     print('---------------')
-    
-    
-async def ch_pr():
-    await client.wait_untill_ready()
-    
-    statuses = ["activity=discord.Game(name='with the ban hammer.'))", "activity=discord.Activity(type=discord.ActivityType.watching, name='{len(client.guilds)} severs!'), 'activity=discord.Activity(type=discord.ActivityType.listening, name="to the moderation team")', 'activity=discord.Game(name="the help command.")']
-    
-    while not client.is_closed():
-        
-        status = random.choice(statuses)
-        
-        await client.change_presence(status)
-        
-        await asyncio.sleep(10)
 
 
 @client.command()
@@ -50,6 +36,5 @@ async def credits(ctx):
     await ctx.send("Thanks to `Code With Swastk` for his tutorials on YouTube")
     await ctx.send("Coded by: from Cr1tical import localidiot#1393")
 
-client.loop.create_task(ch_pr())
 client.run(EDIT THIS AND PUT YOUR TOKEN)
 print('Please edit this file and put your bot token to the above line in the code.')
